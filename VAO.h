18697609deb9e22f1c265683f00d4512bf7c859e
Fish <thehuiq2006@gmail.com>
2025-05-22 16:32:58 -0700

    short gotta use my laptop to make the make file

--- VAO.h
+++ VAO.h
@@ -1,12 +1,18 @@
 #ifndef VAO_CLASS_H
 #define VAO_CLASS_H
 
-#include<glad/glad.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 #include"VBO.h"
 
 class VAO
 {
 	public:
+		glm::vec3 Position;
+		glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+
 		GLuint ID;
 		VAO();
 
